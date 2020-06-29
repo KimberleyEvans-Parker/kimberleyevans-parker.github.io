632b6b5638d06bcfa6416ed4ac1bc626611563b5
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-06-29 13:16:15 +1200

    Modify routing

--- react-multi-page-website/src/App.js
+++ react-multi-page-website/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import { Navigation, Footer, Home, About, Contact } from "./components";
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -10,13 +9,14 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Navigation />
         <Switch>
           <Route path="/" exact component={() => <Home />} />
+          <Route path="/" component={() => <Navigation />} />
+        </Switch>
+        <Switch>
           <Route path="/about" exact component={() => <About />} />
           <Route path="/contact" exact component={() => <Contact />} />
         </Switch>
-        {/* <Footer /> */}
       </Router>
     </div>
   );
