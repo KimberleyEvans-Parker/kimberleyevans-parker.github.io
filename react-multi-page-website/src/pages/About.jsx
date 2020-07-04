cc99be226f73e13b162988207bb78e3a0c7e5a7d
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-04 17:15:25 +1200

    About Page (#17)
    * Create profile-pic.JPG
    
    * Add fade left and right capability
    
    * Removve extra 'container' div
    
    * Add ProfilePic to About page
    
    Also uses Grid component
    
    * Give profile-pic CSS properties
    
    * Add fade in effect
    
    * Add Summary
    
    * Add background
    
    * Add Education
    
    * Add fade in to bottom of About page
    
    * Add more to about summary
    
    * Allow paragraphs to wrap individually by removing Material-UI Grid
    
    * Improve About summary and modify spacing

--- react-multi-page-website/src/pages/About.jsx
+++ react-multi-page-website/src/pages/About.jsx
@@ -1,27 +1,51 @@
 import React from "react";
+import ProfilePic from "../assets/profile-pic.jpg";
 
 function About() {
   return (
-    <div class="content-container">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">About</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
+    <div className="content-container">
+      <img
+        src={ProfilePic}
+        className="profile-pic fade-right"
+        style={{
+          width: "30vw",
+          maxWidth: "200px",
+          float: "left",
+          marginRight: "30px",
+          marginBottom: "20px",
+        }}
+      ></img>
+      <div className="fade-left">
+        <h1 class="font-weight-light">About</h1>
+        <p>
+          I am a problem solver that loves to program. This has lead to a deep
+          interest in robotics and a passion for coding and algorithms. I also
+          enjoy design and I am good at coming up with creative solutions and
+          work well in a team.
+        </p>
+        <p>
+          I'm currently doing Computer Systems Engineering, conjoint with
+          Science; Logic and Computation. During this I'm learning a range of
+          programming languages, as well as logic, mathematics, proofs and
+          electrical systems.
+        </p>
+        <h2>Background</h2>
+        <p>
+          I was born in New Zealand and enjoy travelling. I also enjoy
+          badminton, skiing, art and piano. I am now studying a conjoint of
+          Computer Systems Engineering and Science, majoring in Logic and
+          Computation at the University of Auckland.
+        </p>
+        <h2>Education</h2>
+        <p>
+          I passed NCEA Level 3 with excellence endorsement. I attained 72
+          excellence credits, achieving a rank score of 312/320.
+        </p>
+        <p>
+          I am now in my fourth year of university, taking a conjoint in
+          Computer Systems Engineering and Science, majoring in Logic and
+          Computation. I expect to finish this degree in 2021.
+        </p>
       </div>
     </div>
   );
