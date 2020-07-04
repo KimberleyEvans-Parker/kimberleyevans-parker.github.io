5b2131e17495108ebd21ec15cba6edcdfe13b293
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-04 16:44:42 +1200

    Add more to about summary

--- react-multi-page-website/src/pages/About.jsx
+++ react-multi-page-website/src/pages/About.jsx
@@ -6,14 +6,22 @@ function About() {
   return (
     <div className="content-container">
       <Grid container spacing={4}>
-        <Grid item xs={6} sm={4} md={3} className="fade-right">
+        <Grid item xs={5} sm={4} md={3} className="fade-right">
           <img src={ProfilePic} className="profile-pic"></img>
         </Grid>
         <Grid item xs className="fade-left">
           <h1 class="font-weight-light">About</h1>
           <p>
             I am a problem solver that loves to program. This has lead to a deep
-            interest in coding, robotics and algorithms.
+            interest in coding, robotics and algorithms. I also enjoy design and
+            am good at coming up with creative solutions and work well in a
+            team.
+          </p>
+          <p>
+            I'm currently doing Computer Systems Engineering, conjoint with
+            Science; Logic and Computation. During this I'm learning a range of
+            programming languages, as well as logic, mathematics, proofs and
+            electrical systems.
           </p>
         </Grid>
       </Grid>
