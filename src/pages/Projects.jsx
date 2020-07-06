af0ff3771bceb1d9c5e92ad152dd14eab4e5fbdf
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-06 23:10:28 +1200

    Change Asha's Quest description

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,7 +16,11 @@ function Projects() {
         heading="Doto"
         subheading="Calendar and To-Do App"
         dates="March 2020 - May 2020"
-        description="An open-source software project for an online smart scheduling calendar and to-do app. It has the basic functionality of any calendar app and can be used to make to-do lists. It also has smart scheduling capabilities, meaning the app will allocate inputted tasks in a suitable time in the user’s calendar. Doto uses Google to sign up, meaning that to use this app, the user must have a google account. The development of Doto is done using the M.E.R.N (MongoDB, Express, React, Node) tech stack."
+        description="An open-source software project for an online smart scheduling calendar and to-do app. 
+          It has the basic functionality of any calendar app and can be used to make to-do lists. 
+          It also has smart scheduling capabilities, meaning the app will allocate inputted tasks in a suitable time in the user’s calendar. 
+          Doto uses Google to sign up, meaning that to use this app, the user must have a google account. 
+          The development of Doto is done using the M.E.R.N (MongoDB, Express, React, Node) tech stack."
         link="https://github.com/se701g2/Doto"
         images={[Doto1, Doto2]}
         animationDelay="0s"
@@ -26,7 +30,10 @@ function Projects() {
         heading="Asha's Quest"
         subheading="Java Dungeon Crawler"
         dates="March 2020 - June 2020"
-        description="Roll for Reaction is a turn-base dungeon crawler RPG written with React, Redux, JavaScript and CSS. It is an open-source fork of react-rpg and builds upon and enhances the base game. This is done by modifying and adding elements to lightly emulate the popular tabletop role-playing game Dungeons and Dragons: 5th Edition."
+        description="Asha's Quest is an action-adventure game in the vein of Zelda, created using Java, primarily Java Swing. 
+          This was a 2-person project, with the game created from scratch. 
+          Details of the project are available on the GitHub repository it was cloned to, shown through a GitHub Wiki. 
+          Proficiency in Java 8 (particularly Java Swing and Java AWT) and Git developed."
         link="https://github.com/Matteas-Eden/roll-for-reaction"
         images={[AshasQuest1, AshasQuest2]}
         animationDelay="0.4s"
@@ -37,7 +44,8 @@ function Projects() {
         subheading="Media Feed Aggregator"
         dates="March 2020"
         description="A webapp for aggregrating media feeds from various services into a single, uniform display for user convenience.
-          We are using the React framework to manage the front-end UI and using a Java server utilising the Spring framework for the back-end. The back-end will call the API for the relevant media service and the front-end will visualise this data."
+          We are using the React framework to manage the front-end UI and using a Java server utilising the Spring framework for the back-end. 
+          The back-end will call the API for the relevant media service and the front-end will visualise this data."
         link="https://github.com/softeng-701-group-5/softeng-701-assignment-1"
         // images={[AshasQuest1, AshasQuest2]}
         animationDelay="0.8s"
