de6a8b684d34f88ea32995f62d8c77eca3b58ec9
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-09 22:57:20 +1200

    Route to 404 page when link not found

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
+import Error404 from "./pages/404";
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         <Route path="/about" exact component={About} />
         <Route path="/projects" exact component={Projects} />
         <Route path="/contact" exact component={Contact} />
+        <Route path="/" exact />
+        <Route component={Error404} />
       </Switch>
     </Router>
   );
