a0573b00e1362a474b90116f7c9251ffbe56a860
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-11 00:54:43 +1200

    Remove redundant code

--- src/pages/components/Modal.jsx
+++ src/pages/components/Modal.jsx
@@ -1,33 +1,24 @@
 // import React from "react";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 function Modal(props) {
   const [open, setOpen] = useState(false);
   return (
     <>
-      {/* <!-- Trigger the Modal --> */}
       <img
-        id="myImg"
-        // src="img_snow.jpg"
         src={props.image}
         alt="Snow"
-        // className="carousal_img fade left"
+        className="clickable"
         style={{ width: "100%", maxWidth: "300px" }}
         onClick={() => setOpen(!open)}
       />
 
-      {!open && (
+      {open && (
         <>
-          {/* <img alt="" src={props.image} className="fade left" width="100%" /> */}
-          {/* <img alt="" src={props.image} className="modal-content" /> */}
-          <div
-            // id="myModal"
-            class="modal2"
-          >
+          <div class="modal-background">
             <span class="close" onClick={() => setOpen(!open)}>
               &times;
             </span>
-            {/* <img className="modal-content" id="img01" alt="Snow" /> */}
             <img alt="" src={props.image} className="modal-content" />
             {props.caption && (
               <div className="caption">
@@ -37,28 +28,6 @@ function Modal(props) {
           </div>
         </>
       )}
-      {/* <script>
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-</script> */}
     </>
   );
 }
