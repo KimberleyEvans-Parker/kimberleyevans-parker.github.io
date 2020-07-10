74aa9e8c7257756d4e7737d99be2cd72a8e399dc
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-10 17:59:51 +1200

    Remove Contact page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navigation from "./pages/Navigation";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import Error404 from "./pages/404";
 
@@ -18,7 +17,6 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/about" exact component={About} />
         <Route path="/projects" exact component={Projects} />
-        <Route path="/contact" exact component={Contact} />
         <Route component={Error404} />
       </Switch>
     </Router>
