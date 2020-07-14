54b25633014b49147df5441e87cbcdf825b4bc8b
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-14 22:14:07 +1200

    Give more meaningful caption names for Asha's Quest and Doto images

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import AshasQuest1 from "../assets/AshasQuest1.jpg";
-import AshasQuest2 from "../assets/AshasQuest2.jpg";
-import Doto1 from "../assets/Doto1.gif";
-import Doto2 from "../assets/Doto2.gif";
+import AshasQuest1 from "../assets/Asha's Quest logo.jpg";
+import AshasQuest2 from "../assets/Asha's Quest title screen.jpg";
+import Doto1 from "../assets/Doto marketplace.gif";
+import Doto2 from "../assets/Doto toggle tasks.gif";
 // import RollForReaction from "../assets/Roll for Reaction.mp4";
 
 import Project from "./components/Project";
