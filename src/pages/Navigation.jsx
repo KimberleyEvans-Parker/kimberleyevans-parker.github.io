82b279a34516b8aa3af550269a775d73fe20285d
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-15 23:38:37 +1200

    Add CSS to heading

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -3,13 +3,14 @@ import { Link, withRouter } from "react-router-dom";
 
 function Navigation(props) {
   return (
-    <div className="navigation">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
+    // <div className="navigation">
+    <nav className="navigation">
+      {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark"> */}
+      <div className="container">
+        {/* <Link className="float-left" to="/">
             Kimberley Evans-Parker
-          </Link>
-          <button
+          </Link> */}
+        {/* <button
             className="navbar-toggler"
             type="button"
             data-toggle="collapse"
@@ -19,49 +20,49 @@ function Navigation(props) {
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ml-auto">
-              <li
-                className={`nav-item  ${
-                  props.location.pathname === "/about" ? "active" : ""
-                }`}
-              >
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li
-                className={`nav-item  ${
-                  props.location.pathname === "/projects" ? "active" : ""
-                }`}
-              >
-                <Link className="nav-link" to="/projects">
-                  Projects
-                </Link>
-              </li>
-              <li className={`nav-item`}>
-                <a href="https://github.com/KimberleyEvans-Parker">
-                  <i
-                    className="fa fa-github"
-                    aria-hidden="true"
-                    style={{
-                      fontSize: "24px",
-                      margin: "6px",
-                    }}
-                  ></i>
-                </a>
-              </li>
-              <li className={`nav-item`}>
-                <a href="https://www.linkedin.com/in/kimberley-evans-parker/">
-                  <i className="fa fa-linkedin" aria-hidden="true"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+          </button> */}
+        {/* <div className="collapse navbar-collapse" id="navbarResponsive"> */}
+        <ul>
+          <li
+            className={`nav-item  ${
+              props.location.pathname === "/about" ? "active" : ""
+            }`}
+          >
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+          <li
+            className={`  ${
+              props.location.pathname === "/projects" ? "active" : ""
+            }`}
+          >
+            <Link className="nav-link" to="/projects">
+              Projects
+            </Link>
+          </li>
+          <li className={`nav-item`}>
+            <a href="https://github.com/KimberleyEvans-Parker">
+              <i
+                className="fa fa-github"
+                aria-hidden="true"
+                style={{
+                  fontSize: "24px",
+                  margin: "6px",
+                }}
+              ></i>
+            </a>
+          </li>
+          <li className={`nav-item`}>
+            <a href="https://www.linkedin.com/in/kimberley-evans-parker/">
+              <i className="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* </div> */}
+    </nav>
+    // </div>
   );
 }
 
