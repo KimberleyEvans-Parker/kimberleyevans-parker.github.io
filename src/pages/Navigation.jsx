9422a416557fc11afa5f48f11c5076f8822518b9
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-23 02:17:38 +1200

    Switch to hamburger menu when window width is too small

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -4,37 +4,65 @@ import { Link, withRouter } from "react-router-dom";
 function Navigation(props) {
   const [dropdownOpen, setdropdownOpen] = useState(false);
 
+  /* Keeps track of the window dimensions.  Updates when window resizes */
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+  React.useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+      if (dimensions.width >= 600) {
+        setdropdownOpen(false);
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+    return (_) => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
     <div className="navigation">
       <nav>
         {/* <nav className="navbar navbar-expand-sm navbar-dark bg-dark"> */}
         <div className="navbar-container">
           <ul>
-            <Link to="/">
+            <Link to="/" onClick={() => setdropdownOpen(false)}>
               <li>
                 {/* <li className={dropdownOpen && "dropdown-open"}> */}
                 Kimberley Evans-Parker
               </li>
             </Link>
-            <div
-              className={`navbar-rightside`}
-              onClick={() => setdropdownOpen(!dropdownOpen)}
-            >
-              <li style={{ height: "100%", paddingBottom: "10.5px" }}>
-                <div class={`${dropdownOpen && "change"} menu-icon`}>
-                  <div class="bar1"></div>
-                  <div class="bar2"></div>
-                  <div class="bar3"></div>
-                </div>
-              </li>
-            </div>
+            {dimensions.width < 600 && (
+              <div
+                className={`navbar-rightside`}
+                onClick={() => setdropdownOpen(!dropdownOpen)}
+              >
+                <li style={{ height: "100%", paddingBottom: "10.5px" }}>
+                  <div class={`${dropdownOpen && "change"} menu-icon`}>
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                  </div>
+                </li>
+              </div>
+            )}
           </ul>
-          {dropdownOpen && (
+          {(dropdownOpen || dimensions.width >= 600) && (
             <div
               className={`navbar-rightside ${dropdownOpen && "dropdown-open"}`}
             >
               <ul>
-                <Link className="nav-link" to="/about">
+                <Link
+                  className="nav-link"
+                  to="/about"
+                  onClick={() => setdropdownOpen(false)}
+                >
                   <li
                     className={`${dropdownOpen && "dropdown-open"} ${
                       props.location.pathname === "/about" ? "active" : ""
@@ -43,7 +71,11 @@ function Navigation(props) {
                     About
                   </li>
                 </Link>
-                <Link className="nav-link" to="/projects">
+                <Link
+                  className="nav-link"
+                  to="/projects"
+                  onClick={() => setdropdownOpen(false)}
+                >
                   <li
                     className={`${dropdownOpen && "dropdown-open"} ${
                       props.location.pathname === "/projects" ? "active" : ""
@@ -52,7 +84,10 @@ function Navigation(props) {
                     Projects
                   </li>
                 </Link>
-                <a href="https://github.com/KimberleyEvans-Parker">
+                <a
+                  href="https://github.com/KimberleyEvans-Parker"
+                  onClick={() => setdropdownOpen(false)}
+                >
                   <li className={dropdownOpen && "dropdown-open"}>
                     <i
                       className="fa fa-github"
@@ -64,7 +99,10 @@ function Navigation(props) {
                     ></i>
                   </li>
                 </a>
-                <a href="https://www.linkedin.com/in/kimberley-evans-parker/">
+                <a
+                  href="https://www.linkedin.com/in/kimberley-evans-parker/"
+                  onClick={() => setdropdownOpen(false)}
+                >
                   <li className={dropdownOpen && "dropdown-open"}>
                     <i className="fa fa-linkedin" aria-hidden="true"></i>
                   </li>
