9d82f437282c6fe725391cc863a0d0c381d512a3
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-07-23 01:34:31 +1200

    Add permanant hamburger menu

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -10,63 +10,68 @@ function Navigation(props) {
         {/* <nav className="navbar navbar-expand-sm navbar-dark bg-dark"> */}
         <div className="navbar-container">
           <ul>
-            <li className={dropdownOpen && "dropdown-open"}>
+            <li>
+              {/* <li className={dropdownOpen && "dropdown-open"}> */}
               <Link to="/">Kimberley Evans-Parker</Link>
             </li>
+            <div className={`navbar-rightside`}>
+              <li
+                // className={`${dropdownOpen && "dropdown-open"} clickable`}
+                onClick={() => setdropdownOpen(!dropdownOpen)}
+                style={{ height: "100%", paddingBottom: "10.5px" }}
+              >
+                <div class={`${dropdownOpen && "change"} menu-icon`}>
+                  <div class="bar1"></div>
+                  <div class="bar2"></div>
+                  <div class="bar3"></div>
+                </div>
+              </li>
+            </div>
           </ul>
-          {/* <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button> */}
-          <div
-            className={`navbar-rightside ${dropdownOpen && "dropdown-open"}`}
-          >
-            {/* <div className="collapse navbar-collapse" id="navbarResponsive"> */}
-            <ul>
-              <li
-                className={`${dropdownOpen && "dropdown-open"} ${
-                  props.location.pathname === "/about" ? "active" : ""
-                }`}
-              >
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li
-                className={`${dropdownOpen && "dropdown-open"} ${
-                  props.location.pathname === "/projects" ? "active" : ""
-                }`}
-              >
-                <Link className="nav-link" to="/projects">
-                  Projects
-                </Link>
-              </li>
-              <li className={dropdownOpen && "dropdown-open"}>
-                <a href="https://github.com/KimberleyEvans-Parker">
-                  <i
-                    className="fa fa-github"
-                    aria-hidden="true"
-                    // style={{
-                    //   fontSize: "24px",
-                    //   margin: "6px",
-                    // }}
-                  ></i>
-                </a>
-              </li>
-              <li className={dropdownOpen && "dropdown-open"}>
-                <a href="https://www.linkedin.com/in/kimberley-evans-parker/">
-                  <i className="fa fa-linkedin" aria-hidden="true"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
+          {dropdownOpen && (
+            <div
+              className={`navbar-rightside ${dropdownOpen && "dropdown-open"}`}
+            >
+              {/* <div className="collapse navbar-collapse" id="navbarResponsive"> */}
+              <ul>
+                <li
+                  className={`${dropdownOpen && "dropdown-open"} ${
+                    props.location.pathname === "/about" ? "active" : ""
+                  }`}
+                >
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                </li>
+                <li
+                  className={`${dropdownOpen && "dropdown-open"} ${
+                    props.location.pathname === "/projects" ? "active" : ""
+                  }`}
+                >
+                  <Link className="nav-link" to="/projects">
+                    Projects
+                  </Link>
+                </li>
+                <li className={dropdownOpen && "dropdown-open"}>
+                  <a href="https://github.com/KimberleyEvans-Parker">
+                    <i
+                      className="fa fa-github"
+                      aria-hidden="true"
+                      // style={{
+                      //   fontSize: "24px",
+                      //   margin: "6px",
+                      // }}
+                    ></i>
+                  </a>
+                </li>
+                <li className={dropdownOpen && "dropdown-open"}>
+                  <a href="https://www.linkedin.com/in/kimberley-evans-parker/">
+                    <i className="fa fa-linkedin" aria-hidden="true"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </div>
