6c74aa9905207eab24d6fc668a27fea6a6458b0b
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2020-08-29 01:27:11 +1200

    Move item heading above image and description

--- src/pages/components/ContentItem.jsx
+++ src/pages/components/ContentItem.jsx
@@ -31,112 +31,105 @@ function ContentItem(props) {
   });
 
   return (
-    <Grid container spacing={3} style={{ marginTop: "40px" }}>
-      {(!props.imgOnLeft || dimensions.width < SMALL_SCREEN) && (
-        <Grid
-          item
-          xs
-          className={
-            props.images && dimensions.width >= SMALL_SCREEN
-              ? "fade right"
-              : "fade left"
-          }
-          style={{
-            webkitAnimationDelay: props.animationDelay,
-            mozAnimationDelay: props.animationDelay,
-            oAnimationDelay: props.animationDelay,
-            msAnimationDelay: props.animationDelay,
-            animationDelay: props.animationDelay,
-          }}
-        >
-          {props.link ? (
-            <a href={props.link} className={"popout-link"}>
-              <h2>
-                <i className="fa fa-github popout" aria-hidden="true"></i>
-                {props.heading} {props.subheading && "- "} {props.subheading}
-              </h2>
-            </a>
-          ) : (
-            <h2>
-              {props.heading} {props.subheading && "-"} {props.subheading}
-            </h2>
-          )}
-          <h3>{props.dates}</h3>
-          <p>{props.description}</p>
-        </Grid>
+    <div style={{ marginTop: "40px" }}>
+      {props.link && dimensions.width >= SMALL_SCREEN ? (
+        <a href={props.link} className={"popout-link"}>
+          <h2>
+            <i className="fa fa-github popout" aria-hidden="true"></i>
+            {props.heading} {props.subheading && "- "} {props.subheading}
+          </h2>
+        </a>
+      ) : (
+        <h2>
+          {props.heading} {props.subheading && "-"} {props.subheading}
+        </h2>
       )}
-      {props.images && (
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          md={4}
-          className={
-            props.imgOnLeft && dimensions.width >= SMALL_SCREEN
-              ? "fade right"
-              : "fade left"
-          }
-          style={{
-            webkitAimationDelay: props.animationDelay,
-            mozAnimationDelay: props.animationDelay,
-            oAimationDelay: props.animationDelay,
-            msAnimationDelay: props.animationDelay,
-            animationDelay: props.animationDelay,
-          }}
-        >
-          <Carousel
-            autoPlay
-            showIndicators={props.images.length > 1}
-            showStatus={props.images.length > 1}
-            showThumbs={false}
-            transitionTime={CAROUSEL_TRANSITION_TIME}
-            interval={CAROUSEL_INTERVAL}
-            infiniteLoop={true}
-            className="shadow"
+      <Grid container spacing={3}>
+        {(!props.imgOnLeft || dimensions.width < SMALL_SCREEN) && (
+          <Grid
+            item
+            xs
+            className={
+              props.images && dimensions.width >= SMALL_SCREEN
+                ? "fade right"
+                : "fade left"
+            }
+            style={{
+              webkitAnimationDelay: props.animationDelay,
+              mozAnimationDelay: props.animationDelay,
+              oAnimationDelay: props.animationDelay,
+              msAnimationDelay: props.animationDelay,
+              animationDelay: props.animationDelay,
+            }}
           >
-            {props.images.map((image) => {
-              const path = image.split("/");
-              const name =
-                props.heading + " - " + path[path.length - 1].split(".")[0];
-              return (
-                <button onClick={() => props.openModal(image, name)} key={name}>
-                  <img alt={name} src={image} />
-                </button>
-              );
-            })}
-          </Carousel>
-        </Grid>
-      )}
-      {props.imgOnLeft && dimensions.width >= SMALL_SCREEN && (
-        <Grid
-          item
-          xs
-          className="fade left"
-          style={{
-            webkitAimationDelay: props.animationDelay,
-            mozAnimationDelay: props.animationDelay,
-            oAimationDelay: props.animationDelay,
-            msAnimationDelay: props.animationDelay,
-            animationDelay: props.animationDelay,
-          }}
-        >
-          {props.link ? (
-            <a href={props.link} className={"popout-link"}>
-              <h2>
-                <i className="fa fa-github popout" aria-hidden="true"></i>
-                {props.heading} {props.subheading && "-"} {props.subheading}
-              </h2>
-            </a>
-          ) : (
-            <h2>
-              {props.heading} {props.subheading && "-"} {props.subheading}
-            </h2>
-          )}
-          <h3>{props.dates}</h3>
-          <p>{props.description}</p>
-        </Grid>
-      )}
-    </Grid>
+            <h3>{props.dates}</h3>
+            <p>{props.description}</p>
+          </Grid>
+        )}
+        {props.images && (
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            className={
+              props.imgOnLeft && dimensions.width >= SMALL_SCREEN
+                ? "fade right"
+                : "fade left"
+            }
+            style={{
+              webkitAimationDelay: props.animationDelay,
+              mozAnimationDelay: props.animationDelay,
+              oAimationDelay: props.animationDelay,
+              msAnimationDelay: props.animationDelay,
+              animationDelay: props.animationDelay,
+            }}
+          >
+            <Carousel
+              autoPlay
+              showIndicators={props.images.length > 1}
+              showStatus={props.images.length > 1}
+              showThumbs={false}
+              transitionTime={CAROUSEL_TRANSITION_TIME}
+              interval={CAROUSEL_INTERVAL}
+              infiniteLoop={true}
+              className="shadow"
+            >
+              {props.images.map((image) => {
+                const path = image.split("/");
+                const name =
+                  props.heading + " - " + path[path.length - 1].split(".")[0];
+                return (
+                  <button
+                    onClick={() => props.openModal(image, name)}
+                    key={name}
+                  >
+                    <img alt={name} src={image} />
+                  </button>
+                );
+              })}
+            </Carousel>
+          </Grid>
+        )}
+        {props.imgOnLeft && dimensions.width >= SMALL_SCREEN && (
+          <Grid
+            item
+            xs
+            className="fade left"
+            style={{
+              webkitAimationDelay: props.animationDelay,
+              mozAnimationDelay: props.animationDelay,
+              oAimationDelay: props.animationDelay,
+              msAnimationDelay: props.animationDelay,
+              animationDelay: props.animationDelay,
+            }}
+          >
+            <h3>{props.dates}</h3>
+            <p>{props.description}</p>
+          </Grid>
+        )}
+      </Grid>
+    </div>
   );
 }
 
