e68759eda17c9ffcef3d7a4816f920c834176cb8
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2021-01-30 12:15:28 +1300

    Make second navbar clickable

--- src/pages/Navigation 2.jsx
+++ src/pages/Navigation 2.jsx
@@ -6,37 +6,28 @@ function Navigation2(props) {
       <nav className="nav2 slide-in">
         <div className="navbar-container">
           <ul>
-                <Link
-                  className="nav-link"
-                  to="/experience/work"
-                >
-                  <li
-                    className={`${props.location.pathname === "/experience/work" ? "active" : ""}`}
-                  >
-                    Work
-                  </li>
-                </Link>
-                <Link
-                  className="nav-link"
-                  to="/experience/projects"
-                >
-                  <li
-                    className={`${props.location.pathname === "/experience/projects" ? "active" : ""}`}
-                  >
-                    Projects
-                  </li>
-                </Link>
-                <Link
-                  className="nav-link"
-                  to="/experience/awards"
-                >
-                  <li
-                    className={`${props.location.pathname === "/experience/awards" ? "active" : ""}`}
-                  >
-                    Awards
-                  </li>
-                </Link>
-              </ul>
+            <Link className="nav-link" to="/experience/work">
+              <li
+                className={`${props.location.pathname === "/experience/work" ? "active" : ""}`}
+              >
+                Work
+              </li>
+            </Link>
+            <Link className="nav-link" to="/experience/projects">
+              <li
+                className={`${props.location.pathname === "/experience/projects" ? "active" : ""}`}
+              >
+                Projects
+              </li>
+            </Link>
+            <Link className="nav-link" to="/experience/awards">
+              <li
+                className={`${props.location.pathname === "/experience/awards" ? "active" : ""}`}
+              >
+                Awards
+              </li>
+            </Link>
+          </ul>
         </div>
       </nav>
   );
