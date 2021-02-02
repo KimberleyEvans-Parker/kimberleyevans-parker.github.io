b2c2a661370dfcf133a955fbd59b5aacee912c49
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2021-02-02 21:58:37 +1300

    Make navbar responsive in small screen sizes

--- src/pages/Navigation 2.jsx
+++ src/pages/Navigation 2.jsx
@@ -5,10 +5,30 @@ import SubNavbarLink from "./components/SubNavbarLink";
 
 function Navigation2(props) {
 
+  /* Keeps track of the window dimensions.  Updates when window resizes */
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+  React.useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+    return (_) => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
   return (
       <nav className="nav2 slide-in">
-        <div className="navbar-container">
-          <ul>
+        <div className={`nav2-container ${dimensions.width >= EXTRA_SMALL_SCREEN ? 
+          "navbar-container" : "navbar-container-smallscreen"}`}>
+          <ul className={dimensions.width < EXTRA_SMALL_SCREEN ?? "extra-small-screensize"}>
             <SubNavbarLink heading="Work" />
             <SubNavbarLink heading="Volunteer" />
             <SubNavbarLink heading="Projects" />
