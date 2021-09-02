1445654a8440667eb82ad7e154cef8ac6e52cf60
KimberleyEvans-Parker <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2021-09-02 22:23:58 +1200

    Add subnavbar as a component

--- src/pages/components/SubNavbarLink.jsx
+++ src/pages/components/SubNavbarLink.jsx
@@ -5,12 +5,12 @@ function SubNavbarLink(props) {
   return (
     <Link
       className="nav-link"
-      to={`/experience/` + props.heading.toLowerCase()}
+      to={`/${props.section}/${props.heading.toLowerCase()}`}
     >
       <li
         className={`${
           props.location.pathname ===
-          "/experience/" + props.heading.toLowerCase()
+          `/${props.section}/${props.heading.toLowerCase()}`
             ? "active"
             : ""
         } 
