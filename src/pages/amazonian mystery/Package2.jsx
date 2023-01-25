653c6b5c805c5a8e0d503e69ea3cec80f7553537
Kimberley <kim@evans-parker.com>
2023-01-25 23:19:51 +1300

    Add multiple puzzles with animation delay

--- src/pages/amazonian mystery/Package2.jsx
+++ src/pages/amazonian mystery/Package2.jsx
@@ -2,8 +2,36 @@ import React, { useState } from "react";
 import CodeInput from "./CodeInput";
 import "./styles.css"
 
+function Puzzle(clue, correctAnswer, wait) {
+  const [answeredCorrectly, setAnsweredCorrectly] = useState(false);
+
+  return {
+    test: ":D",
+    clue: clue,
+    correctAnswer: correctAnswer,
+    wait: wait,
+    answeredCorrectly: answeredCorrectly, 
+    setAnsweredCorrectly: setAnsweredCorrectly,
+  }
+}
+
 export default function Package2() {
-    const [foundLocation, setFoundLocation] = useState(false);
+  
+  const puzzles = [
+    Puzzle("Puzzle 1!", "123"),
+    Puzzle("Puzzle 2", "123", 2000),
+    Puzzle("Puzzle 3", "123", 2000),
+    Puzzle("Puzzle 4", "123", 2000),
+    Puzzle("Puzzle 5", "123", 2000),
+  ]
+
+  let animationDelay = 2;
+
+  const getAnimationDelay = () => {
+    const animationDelaySeconds = animationDelay.toString() + "s";
+    animationDelay += 0.4;
+    return animationDelaySeconds;
+  };
   
     return (
       <div className="fade left">
@@ -14,6 +42,18 @@ export default function Package2() {
           We’d better get there as fast as we can.  
         </p>
         <i>Open package 2</i>
+        <br />
+        <br />
+          {puzzles.map((puzzle) => {
+              return <>{puzzle.test}<CodeInput
+                clue={puzzle.clue}
+                correctAnswer={puzzle.correctAnswer}
+                onCorrectAnswer={puzzle.setAnsweredCorrectly}
+                wait={puzzle.wait}
+                animationDelay={getAnimationDelay()}
+              /></>
+            })}
+            {puzzles[0].answeredCorrectly && "Yay"}
       </div>
     );
   }
