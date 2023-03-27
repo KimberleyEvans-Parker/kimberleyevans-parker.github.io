b452fc1d3c60cbb53a98ba81d5d369ab52ceb83b
Kimberley <kim@evans-parker.com>
2023-03-27 22:50:17 +1300

    Put fireflies and animations into their own files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./fireflies.css";
+import "./animations.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
