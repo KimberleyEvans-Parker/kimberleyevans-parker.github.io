9fb03d67f2b14753849e6f57a1496ef4cd6f8bb1
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-02-01 21:46:14 +1300

    Parse words better

--- src/assets/txt-to-json.py
+++ src/assets/txt-to-json.py
@@ -1,13 +1,35 @@
-with open("./src./assets./words.txt", "r") as f:
-    words = f.readlines()
 
-for w in words:
-    if words.count(w) >= 2:
-        print(w.strip())
+def getWordsFromFile(filename):
+    with open(filename, "r") as f:
+        return f.readlines()
+    
 
-with open("./src./assets./words.json", "w") as f:
-    f.write("[")
-    for w in words[:-1]:
-        f.write('"' + w.strip() + '", ')
-    f.write('"' + words[-1].strip() + '"')
-    f.write("]")
+def parseWords(words):
+    new_words = []
+
+    for word in words:
+        if word[0].islower():
+            tmp = word.split()
+            for new_word in tmp:
+                if new_word not in new_words:
+                    new_words.append(new_word)
+
+    return new_words
+
+
+def writeWordsToFile(filename, words):
+    with open(filename, "w") as f:
+        f.write("[\n")
+        for w in words[:-1]:
+            f.write('"' + w.strip() + '",\n')
+        f.write('"' + words[-1].strip() + '"')
+        f.write("]")
+
+
+
+words = getWordsFromFile("./src./assets./words.txt")
+new_words = parseWords(words)
+
+print(new_words[:20])
+
+writeWordsToFile("./src./assets./words.json", new_words)
