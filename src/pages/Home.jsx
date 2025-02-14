4e7add91b1c2cb836d6f30dd96f8ceeddc4c92d8
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-02-14 17:03:26 +1300

    Make entire home page clickable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,12 @@ import { Link, withRouter } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="home-containter">
-      <h1 className="fade down">Kimberley Evans-Parker</h1>
-      <Link className="nav-link" to="/about">
+    <Link className="nav-link" to="/about">
+      <div className="home-containter">
+        <h1 className="fade down">Kimberley Evans-Parker</h1>
         <p className="fade up">Welcome</p>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
 
