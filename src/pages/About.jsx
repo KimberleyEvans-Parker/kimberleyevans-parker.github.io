98dbafa61a0303268b05cccd19e66a7eafac380b
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-03-11 13:47:51 +1300

    Update CV

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProfilePic from "../assets/profile-pic.jpg";
-import Resume from "../assets/CV-24-01.pdf";
+import Resume from "../assets/CV-25-03.pdf";
 import { SMALL_SCREEN } from "../Constants";
 
 function About() {
