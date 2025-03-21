665eb0cef5cf17d61e31b44df7002cd117326b60
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-03-21 13:35:56 +1300

    Data refactor (#216)
    * Move info in project to project data file
    
    * move work info out into work data
    
    * move award info to awardsData
    
    * move volunteer info to volunteerData
    
    * Add missing import
    
    * Remove unused imports

---
+++ src/data/awards.ts
@@ -0,0 +1,72 @@
+import { importAllPhotosFromFolder } from "../helpers/Helpers";
+import { ContentItemType } from "./types";
+
+
+const KrayonImages = importAllPhotosFromFolder(
+    require.context("../assets/awards/2020 DEVS Hackathon", false)
+  );
+  const MYOBImages = importAllPhotosFromFolder(
+    require.context("../assets/awards/MYOB IT Challenge", false)
+  );
+  const CertificateImages = importAllPhotosFromFolder(
+    require.context("../assets/awards/Certificates", false)
+  );
+
+
+export const awardsData: ContentItemType[] = [
+    {
+        heading: "University Certificates of Outstanding Achievment",
+        dates: "2021",
+        description: "I received several certificates of outstanding achievment for my hard work during university.",
+        images: CertificateImages,
+    },
+    {
+        heading: "2020 MYOB IT Challenge",
+        subheading: "3rd place",
+        dates: "September 2020",
+        description: `I competed in the MYOB IT Challenge with a team of 4 against over 140 other teams across Australia and New Zealand.  
+        Our challenge was to come up with an AI-powered solution to a problem encountered during quarantine and isolation.  
+        We decided to focus on the reduced productivity and mental well-being.  
+        To combat this problem, we created Heart Healer - an AI chatbot, friend and counsellor, which you can view on my projects page.
+        During the competition, there were two rounds - the first of which was a video pitch of our idea.  
+        Six successful teams progressed to the second round, where we were able to get feedback, before pitching our idea live to the judges.
+        My team - Whiz Craft - was awarded 3rd place overall, or 1st in New Zealand.`,
+        images: MYOBImages,
+        link: "https://www.auckland.ac.nz/en/news/2020/10/09/heart-healer-app-a-winner.html",
+    },
+    {
+        heading: "2020 DEVS Hackathon",
+        subheading: "1st place",
+        dates: "July 2020",
+        description: `My team of 6 participated in the DEVS 48-hour Hackathon, making a demo platform; Upskill.  
+        We took out market research to validate our idea and performed financial analysis.  
+        In the end, my team was awarded 1st place!  
+        We gained a huge amount of experience, and learnt a lot.  
+        You can also see the project Upskill on my projects page.`,
+        images: KrayonImages,
+    },
+    {
+        heading: "AUCS Hackathon",
+        subheading: "Best Presentation",
+        dates: "May 2019",
+        description:
+            `My team of 6 was awarded best presentation during the 2019 AUCS, KPMG Hackathon.
+            Our presentation included our project; Eventigate.  
+            A platform for connecting event organisers, locations and people, which you can find out more about on my projects page.`,
+    },
+    {
+        heading: "New Zealand Programming Competition",
+        subheading: "3rd in New Zealand",
+        dates: "June 2019",
+        description: `We worked in a team of three to solve problems of a range of difficulties in Python, C or Java.
+            100-point questions, in particular, were designed to be extremely difficult, but my team and I managed to work together to complete the problem.
+            As a result of our hard work, team effort and problem-solving skills, our team ranked 2nd in Auckland and 3rd in New Zealand.`,
+    },
+    {
+        heading: "Alice Programming Competition",
+        subheading: "2nd place",
+        dates: "April 2016",
+        description: `My partner and I received 2nd place for a competition in the Alice programming environment.
+            During this, we were able to create 3D animations to fit a desired script, within a given time frame.`,
+    },
+]
--- src/pages/Awards.jsx
+++ src/pages/Awards.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { importAllPhotosFromFolder } from "../helpers/Helpers";
+import { useState } from "react";
 
 import ContentItem from "./components/content item/ContentItem";
+import { awardsData } from "../data/awards";
 
 function Awards() {
   const [image, setImage] = useState(null);
@@ -27,15 +27,6 @@ function Awards() {
     return onLeft;
   };
 
-  const KrayonImages = importAllPhotosFromFolder(
-    require.context("../assets/awards/2020 DEVS Hackathon", false)
-  );
-  const MYOBImages = importAllPhotosFromFolder(
-    require.context("../assets/awards/MYOB IT Challenge", false)
-  );
-  const CertificateImages = importAllPhotosFromFolder(
-    require.context("../assets/awards/Certificates", false)
-  );
 
   return (
     <div className="content-container experience-container">
@@ -55,78 +46,21 @@ function Awards() {
           </div>
         </>
       )}
-      <ContentItem
-        imgOnLeft={imgOnLeft()}
-        heading="University Certificates of Outstanding Achievment"
-        dates="2021"
-        description="I received several certificates of outstanding achievment for my hard work during university."
-        images={CertificateImages}
-        openModal={openModal}
-        animationDelay={getAnimationDelay()}
-      />
-      <ContentItem
-        imgOnLeft={imgOnLeft()}
-        heading="2020 MYOB IT Challenge"
-        subheading="3rd place"
-        dates="September 2020"
-        description="I competed in the MYOB IT Challenge with a team of 4 against over 140 other teams across Australia and New Zealand.  
-          Our challenge was to come up with an AI-powered solution to a problem encountered during quarantine and isolation.  
-          We decided to focus on the reduced productivity and mental well-being.  
-          To combat this problem, we created Heart Healer - an AI chatbot, friend and counsellor, which you can view on my projects page.
-          During the competition, there were two rounds - the first of which was a video pitch of our idea.  
-          Six successful teams progressed to the second round, where we were able to get feedback, before pitching our idea live to the judges.
-          My team - Whiz Craft - was awarded 3rd place overall, or 1st in New Zealand."
-        images={MYOBImages}
-        link="https://www.auckland.ac.nz/en/news/2020/10/09/heart-healer-app-a-winner.html"
-        openModal={openModal}
-        animationDelay={getAnimationDelay()}
-      />
-      <ContentItem
-        imgOnLeft={imgOnLeft()}
-        heading="2020 DEVS Hackathon"
-        subheading="1st place"
-        dates="July 2020"
-        description="My team of 6 participated in the DEVS 48-hour Hackathon, making a demo platform; Upskill.  
-          We took out market research to validate our idea and performed financial analysis.  
-          In the end, my team was awarded 1st place!  
-          We gained a huge amount of experience, and learnt a lot.  
-          You can also see the project Upskill on my projects page."
-        images={KrayonImages}
-        openModal={openModal}
-        animationDelay={getAnimationDelay()}
-      />
-      <ContentItem
-        imgOnLeft={imgOnLeft()}
-        heading="AUCS Hackathon"
-        subheading="Best Presentation"
-        dates="May 2019"
-        description="My team of 6 was awarded best presentation during the 2019 AUCS, KPMG Hackathon.
-          Our presentation included our project; Eventigate.  
-          A platform for connecting event organisers, locations and people, which you can find out more about on my projects page."
-        openModal={openModal}
-        animationDelay={getAnimationDelay()}
-      />
-      <ContentItem
-        imgOnLeft={imgOnLeft()}
-        heading="New Zealand Programming Competition"
-        subheading="3rd in New Zealand"
-        dates="June 2019"
-        description="We worked in a team of three to solve problems of a range of difficulties in Python, C or Java.  
-          100-point questions, in particular, were designed to be extremely difficult, but my team and I managed to work together to complete the problem.  
-          As a result of our hard work, team effort and problem-solving skills, our team ranked 2nd in Auckland and 3rd in New Zealand."
-        openModal={openModal}
-        animationDelay={getAnimationDelay()}
-      />
-      <ContentItem
-        imgOnLeft={imgOnLeft()}
-        heading="Alice Programming Competition"
-        subheading="2nd place"
-        dates="April 2016"
-        description="My partner and I received 2nd place for a competition in the Alice programming environment.
-          During this, we were able to create 3D animations to fit a desired script, within a given time frame."
-        openModal={openModal}
-        animationDelay={getAnimationDelay()}
-      />
+      {awardsData.map((project, index) => (
+        <ContentItem
+          key={index}
+          imgOnLeft={imgOnLeft()}
+          heading={project.heading}
+          subheading={project.subheading}
+          dates={project.dates}
+          description={project.description}
+          githubLink={project.githubLink}
+          images={project.images}
+          technologies={project.technologies}
+          openModal={openModal}
+          animationDelay={getAnimationDelay()}
+        />
+      ))}
     </div>
   );
 }
