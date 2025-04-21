93a54a1b7ce3c0bf4ad5f0e62869fbb1eabcf39c
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-04-21 16:03:23 +0100

    Convert TextContent to tsx

---
+++ src/pages/components/content item/TextContent.tsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
+
+
+export const TextContent = (
+  props: {
+    dates: string | null | undefined;
+    description: string | null | undefined;
+    teamSize: number | undefined;
+    linkedInLink: string | undefined;
+    link: string | undefined;
+    projectLink: any;
+    technologies: any[];
+  }
+) => {
+  return (
+    <>
+      <h3>
+        {props.dates}
+      </h3>
+      <p>
+        {props.description}
+      </p>
+      {props.teamSize && (
+        <p>
+          Team size: {props.teamSize}
+        </p>
+      )}
+      {props.linkedInLink && (
+        <a href={props.linkedInLink} className={"popout-link"}>
+          <i className="fa fa-linkedin popout" aria-hidden="true" />
+          <p>
+            See the LinkedIn post here!
+          </p>
+        </a>
+      )}
+      {props.link && (
+        <p>
+          <a href={props.link}>
+            For more information, take a look here
+          </a>
+        </p>
+      )}
+      {props.projectLink && (
+        <p>
+          <Link to={`/experience/projects#${props.projectLink}`}>
+            See the project here
+          </Link>
+        </p>
+      )}
+      <div className="technologies">
+        {props.technologies && props.technologies.join(` ⸎ `)}
+      </div>
+    </>
+  );
+}
