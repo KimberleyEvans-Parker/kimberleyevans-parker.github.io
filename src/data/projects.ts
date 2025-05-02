cd14136e702c959ed11b519bd998c315b9bdbb8e
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-05-02 23:50:34 +0100

    Update all dates data to use new data type

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -40,7 +40,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Empower",
         subheading: "Network to Empower Women",
-        dates: "July 2022",
+        dates: { start: new Date(2022, 6) },
         description: "I was invited to the We the Women Hackfest, run by Girlboss. During this, my team of five focused on ways we could help women dependent on others for a living.  We created Empower.  A network of support for women to provide resources and people so that they can regain their independence.  ",
         teamSize: 5,
         links: { github: "https://github.com/Eva-Sorensen/empower" },
@@ -50,7 +50,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Industry 4.0",
         subheading: "Machine Health Monitoring Software",
-        dates: "August 2020 - November 2020",
+        dates: { start: new Date(2020, 7), end: new Date(2020, 10) },
         description: "A cross-platform application made by 8 people following agile practices.  The application was able to take data from different machines and sensors and display them to the technicians.  Users would be able to view this data in real-time, and see historical data, and receive notifications when a machine reported unhealthy readings.  The project which we started for our Product Owner has received funding to continue its production.  ",
         teamSize: 8,
         links: { github: "https://github.com/KimberleyEvans-Parker/industry-4" },
@@ -60,7 +60,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Heart Healer",
         subheading: "AI Counsellor",
-        dates: "September 2020",
+        dates: { start: new Date(2020, 8) },
         description: "This was conceptualised for the 2020 MYOB IT challenge, in which the problems of isolation and quarantine were highlighted.  To combat the problems that arose through isolation and quarantine, we created Heart Healer.  Heart Healer is an AI chatbot and counsellor that monitors your mental wellbeing and productivity.  It can schedule in breaks for you, talk to you when you're lonely and connect you to other people.  This chatbot would be able to learn individual's preferences through an AI backend and connect people to their business or personal counsellor if needed.",
         teamSize: 4,
         images: HeartHealerImages
@@ -68,7 +68,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Upskill",
         subheading: "Platform to connect and upskill",
-        dates: "July 2020",
+        dates: { start: new Date(2020, 6) },
         description: "Upskill was made during the DEVS 2020 hackathon, in only 48 hours.  It is a a platform for live learning and teaching, where you connect and network with like-minded locals.  The platform also serves as a place for companies to post challenges and recruit bright minds.  Upskill aims to help those impacted by Covid 19, by allowing them to upskill and find new job opportunities.  ",
         teamSize: 6,
         links: { github: "https://github.com/Pyxxil/upskill" },
@@ -78,7 +78,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Roll for Reaction",
         subheading: "Turn-Based Dungeon Crawler RPG",
-        dates: "March 2020 – June 2020",
+        dates: { start: new Date(2020, 2), end: new Date(2020, 5) },
         description: "Roll for Reaction is a turn-based dungeon crawler RPG written with React, Redux, JavaScript, HTML and CSS. It is an open-source fork of react-rpg and builds upon and enhances the base game. This is done by modifying and adding elements to lightly emulate the popular tabletop role-playing game Dungeons and Dragons: 5th Edition.",
         teamSize: 4,
         links: { github: "https://github.com/Matteas-Eden/roll-for-reaction" },
@@ -88,7 +88,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Doto",
         subheading: "Calendar and To-Do App",
-        dates: "March 2020 - May 2020",
+        dates: { start: new Date(2020, 2), end: new Date(2020, 4) },
         description: "An open-source software project for an online smart scheduling calendar and to-do app. It has the basic functionality of any calendar app and can be used to make to-do lists. It has smart scheduling capabilities, meaning the app will allocate inputted tasks in a suitable time in the user’s calendar. Doto uses Google auth to allow people to sign in. The development of Doto is done using the M.E.R.N (MongoDB, Express, React, Node) tech stack.",
         teamSize: 20,
         links: { github: "https://github.com/se701g2/Doto" },
@@ -98,7 +98,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Feedr",
         subheading: "Media Feed Aggregator",
-        dates: "March 2020",
+        dates: { start: new Date(2020, 2) },
         description: "A webapp for aggregating media feeds from various services into a single, uniform display for user convenience. We are using the React framework to manage the front-end UI and using a Java server utilising the Spring framework for the back-end. The back-end will call the API for the relevant media service and the front-end will visualise this data.",
         teamSize: 20,
         links: { github: "https://github.com/softeng-701-group-5/softeng-701-assignment-1" },
@@ -106,18 +106,18 @@ export const projectData: ContentItemType[] = [
         technologies: [TECHNOLOGIES.GIT, TECHNOLOGIES.HTML, TECHNOLOGIES.CSS, TECHNOLOGIES.JS, TECHNOLOGIES.REACT, TECHNOLOGIES.REDUX]
     },
     {
-        heading: "A Little Midgnight Music",
+        heading: "A Little Midnight Music",
         subheading: "Research into Automatic Music Transcription",
-        dates: "March 2020 - November 2020",
-        description: "My partner and I did our part 4 research topic into the Automatic transcription of piano music.  During this project, research was conducted into Fourier Transform, STFT, Constant-Q, Onset/Beat Tracking, Machine Learning: Artificial, Convolutional and Recurrent Neural Networks.  Our neural network was an extension of the wav2mid project Jon Sleep created and converted wav files into the more computer-readible midi files.  ",
+        dates: { start: new Date(2020, 2), end: new Date(2020, 10) },
+        description: "My partner and I did our part 4 research topic into the Automatic transcription of piano music.  During this project, research was conducted into Fourier Transform, STFT, Constant-Q, Onset/Beat Tracking, Machine Learning: Artificial, Convolutional and Recurrent Neural Networks.  Our neural network was an extension of the wav2mid project Jon Sleep created and converted wav files into the more computer-readable midi files.  ",
         teamSize: 2,
         links: { github: "https://github.com/KimberleyEvans-Parker/wav2mid" },
         technologies: [TECHNOLOGIES.GIT, TECHNOLOGIES.PYTHON, TECHNOLOGIES.AI]
     },
     {
         heading: "Envirobot",
-        subheading: "Platofrmer Game",
-        dates: "Febraury 2020",
+        subheading: "Platformer Game",
+        dates: { start: new Date(2020, 1) },
         description: "Created for the Global Game Jam, by a team of 3.  This is set in the near future, where a robot aims to heal the planet by planting trees, putting out fires and repairing structures.  ",
         teamSize: 3,
         links: { github: "https://github.com/Pyxxil/GameJam" },
@@ -127,7 +127,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Flik",
         subheading: "Line Following Robot",
-        dates: "July 2019 - Oct 2019",
+        dates: { start: new Date(2019, 6), end: new Date(2019, 9) },
         description: "A line-following robot that utilises a custom-made PCB with light sensors, a radio frequency module, ADC and PSoC.  Various shortest path algorithms were created for the robot to plan its traversal across the map, which were written in C.",
         teamSize: 4,
         links: { github: "https://github.com/Matteas-Eden/flik" },
@@ -137,7 +137,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Midnight's Messages",
         subheading: "CherryPy Social Network",
-        dates: "May 2019 - June 2019",
+        dates: { start: new Date(2019, 4), end: new Date(2019, 5) },
         description: "A social network similar to Twitter, which was developed individually in Python 3, on a CherryPy server.  It also utilises NaCl, json, jinja 2, HTML5 and CSS, JavaScript and cryptography.  This could authorize and report login from a login server to a main server run by the lecturer, and see how many web clients are online.  It could also send messages privately and in group chats while sending public 'tweets' out to all viewers, and block keywords and people from showing up. ",
         teamSize: 1,
         technologies: [TECHNOLOGIES.PYTHON, TECHNOLOGIES.HTML, TECHNOLOGIES.CSS, TECHNOLOGIES.JS]
@@ -145,7 +145,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Eventigate",
         subheading: "Project During KPMG Hackathon",
-        dates: "May 2019",
+        dates: { start: new Date(2019, 4) },
         description: `A hub for people to organise and meet up at a range of events with a specified host and location.  
             Aims to connect organisations and volunteer groups who wish to hold events with places people.  
             This was done by 6 people using JavaScript, HTML and CSS. 
@@ -158,16 +158,16 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Kalah",
         subheading: "The Game Mancala",
-        dates: "May 2020",
+        dates: { start: new Date(2020, 4) },
         description: "This is the game Mancala written in Java.  It was designed to promote good programming qualities such as modularity, modifiability and extendability.",
         teamSize: 1,
         links: { github: "https://github.com/KimberleyEvans-Parker/kalah" },
         technologies: [TECHNOLOGIES.GIT, TECHNOLOGIES.JAVA]
-    }, 
+    },
     {
         heading: "Scammr",
         subheading: "Phone Scam Detector",
-        dates: "July 2019",
+        dates: { start: new Date(2019, 6) },
         description: "This app was developed during the 2019, 48-hour Devs Hackathon by a group of 6.  The app listened in to a call and warned you if it thought the caller was trying to scam you.  Sammr makes use of speech-to-text tools and an algorithm to decide the probability of the call being a scam.",
         teamSize: 6,
         links: { github: "https://github.com/Pyxxil/Scammr" },
@@ -176,7 +176,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Flappy Bird",
         subheading: "FPGA VHDL Side Scroller Game",
-        dates: "April 2019 - May 2019",
+        dates: { start: new Date(2019, 3), end: new Date(2019, 4) },
         description: "A game based off Flappy Bird, created by a team of 2, using VHDL.  This was designed to be run on an Altera DE0 Board, equipped with an Altera Cyclone III 3C16 FPGA.  Understanding of the FPGA hardware, VHDL and Git were developed.",
         teamSize: 2,
         technologies: [TECHNOLOGIES.VHDL]
@@ -184,7 +184,7 @@ export const projectData: ContentItemType[] = [
     {
         heading: "Asha's Quest",
         subheading: "Java Dungeon Crawler",
-        dates: "March 2019 - May 2019",
+        dates: { start: new Date(2019, 2), end: new Date(2019, 4) },
         description: "Asha's Quest is an action-adventure game in the vein of Zelda, created using Java, primarily Java Swing.  This was a 2-person project, with the game created from scratch.  Details of the project are available on the GitHub repository it was cloned to, shown through a GitHub Wiki.  Proficiency in Java 8 (particularly Java Swing and Java AWT) and Git developed.",
         teamSize: 2,
         links: { github: "https://github.com/KimberleyEvans-Parker/AshasQuest" },
@@ -193,9 +193,9 @@ export const projectData: ContentItemType[] = [
     },
     {
         heading: "Wireless Energy Monitor",
-        dates: "Aug 2018 – Nov 2018",
+        dates: { start: new Date(2018, 7), end: new Date(2018, 10) },
         description: "Wireless Energy Monitor to display voltage, current and power for ELECTENG 209.  Comprised of a digital and an analogue section. Data receiver described in VHDL, transmitter programmed in C.  Receiver was a CPLD, transmitter was an ATMEGA328PB microcontroller on a self- designed PCB.  Breadboarding, LTSpice simulations and Oscilloscopes were used for testing.",
         teamSize: 2,
         technologies: [TECHNOLOGIES.VHDL]
     }
-]
+];
--- src/data/volunteer.ts
+++ src/data/volunteer.ts
@@ -34,7 +34,7 @@ export const volunteerData: ContentItemType[] = [
     {
         heading: "Teacher",
         subheading: "Take2",
-        dates: "February 2025",
+        dates: { start: new Date(2025, 1) },
         description: `I volunteered to help with the Take2 program, which is a program that helps people in the New Zealand Justice System with a second chance.  
             Xero is a partner of the program and I was able to help with the program by giving the students a fun course on CSS.  
             I was a teacher for a group of students who were struggling with their studies and needed extra help. 
@@ -48,7 +48,7 @@ export const volunteerData: ContentItemType[] = [
     {
         heading: "Reforestation",
         subheading: "Motutapu Island", 
-        dates: "July 2024",
+        dates: { start: new Date(2024, 6) },
         description: `I volunteered to help with the reforestation and conservation on Motutapu Island.  
             This involved planting native trees and shrubs to help restore the island to its natural state.
             The island is a predator-free sanctuary for native birds and lizards, and the work we did will help to ensure that it remains that way.`,
@@ -60,7 +60,7 @@ export const volunteerData: ContentItemType[] = [
     {
         heading: "Growing Sapplings",
         subheading: "Motuihe Island",
-        dates: "Feb 2023",
+        dates: { start: new Date(2023, 1) },
         description: `Today we were working in the nursery, potting up seedlings and sowing seeds.
             The seedlings will be planted out on the island later, and the seeds will be grown on to become seedlings for planting next year.
             I think now I have been involved in every stage of the process of reforestation on the island!`,
@@ -69,7 +69,7 @@ export const volunteerData: ContentItemType[] = [
     {
         heading: "Speaker at a Queer&A Event",
         subheading: "University of Auckland", 
-        dates: "July 2023",
+        dates: { start: new Date(2023, 6) },
         description: "I spoke at the Rainbow Engineering UoA and Women in Engineering Network Event about what the software industry was like for minorities.",
         links: {
             linkedIn: "https://www.linkedin.com/posts/aryankaran_it-was-a-pleasure-to-mc-the-rainbow-engineering-ugcPost-7057242406928224257-7NGU",
@@ -79,7 +79,7 @@ export const volunteerData: ContentItemType[] = [
     {
         heading: "Conservation",
         subheading: "McDonalds' Farm",
-        dates: "March 2023",
+        dates: { start: new Date(2023, 2) },
         description: `I volunteered to help with the conservation of the McDonalds' Farm in Auckland.  
             This involved removing weeds that were choking the native plants to give the natives the best chance of survival.
             The trees are an important part of the river ecosystem and the work we did will help to ensure that the river remains healthy.`,
@@ -91,7 +91,7 @@ export const volunteerData: ContentItemType[] = [
     {
         heading: "Volunteer Teahcer",
         subheading: "Tauranga STEMfest",
-        dates: "October 2024",
+        dates: { start: new Date(2024, 9) },
         description: `I  volunteered to help with a stall at the Tauranga STEMfest.
             This involved helping to teach children about coding and robotics, and how they can be used to solve real-world problems.  
             The students were able to use LEGO Mindstorm robots to navigate their way around a maze, and I was able to help them with their coding skills.`,
@@ -103,7 +103,7 @@ export const volunteerData: ContentItemType[] = [
     {
         heading: "Mentor",
         subheading: "Women in Tech",
-        dates: "2022",
+        dates: { start: new Date(2022) },
         description: "I mentored a student taking a conjoint at the university of Auckland into the STEM industry through the Women in Tech program.",
         links: {
             seeMore: "https://wen.auckland.ac.nz/",
@@ -112,7 +112,7 @@ export const volunteerData: ContentItemType[] = [
     {
         heading: "Conservation",
         subheading: "Motuihe Island",
-        dates: "March 2022",
+        dates: { start: new Date(2022, 2) },
         description: `I went back to Motuihe Island to help with the conservation of the island! 
         It was amazing to see the trees that I had planted in 2015 had grown so much and were now becoming a stong and healthy forest.
         This time our job was to remove the weeds from around the island and clear the pathways to make it easier for the public to walk around the island.`,
@@ -121,30 +121,30 @@ export const volunteerData: ContentItemType[] = [
     {
         heading: "Class Rep",
         subheading: "Phil 222 and Logicomp 301",
-        dates: "July 2019 - July 2020",
+        dates: { start: new Date(2019, 6), end: new Date(2020, 6) },
         description: "I represented the students in my class and communicate any questions, queries, concerns or feedback to the teacher.",
         images: ClassRepImages,
     },
     {
         heading: "Robogals",
         subheading: "STEM Teacher",
-        dates: "April 2019 - July 2019",
+        dates: { start: new Date(2019, 3), end: new Date(2019, 6) },
         description: `I worked as a teacher with Robogals to encourage young children into STEM fields.  
         This was done with the aid of LEGO Mindstorm robots, which were able to be programmed by the children to navigate their way around a maze.`,
     },
     {
         heading: "Reforestation",
         subheading: "Motuihe Island",
-        dates: "August 2015",
+        dates: { start: new Date(2015, 7) },
         description: `Motuihe Island used to be farmland, but we have helped transform it into a home for native birds and lizards.  
         During a restoration project as part of the Motuihe Island Restoration Trust, we planted 2,000 native trees around the island to help make the island a sanctuary for wild life`,
         images: Motuihe15Images,
     },
     {
         heading: "Poppy Making",
-        dates: "April 2015",
+        dates: { start: new Date(2015, 3) },
         description: `I volunteered to help a small group of people make hundreds of poppies, sewn out of red fabric and buttons. 
         These were placed in the gardens of our school and given away as part of the ANZAC commemoration.  `,
         images: PoppyImages,
     },
-]
+];
