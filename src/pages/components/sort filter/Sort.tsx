c5be3e6a7d40e0db18bead4cd950e79163c260a0
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-05-03 00:11:32 +0100

    Move sorting into a new helper file

--- src/pages/components/sort filter/Sort.tsx
+++ src/pages/components/sort filter/Sort.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { ContentItemType } from "../../../data/types";
 import "./sort.css";
+import { sortByEndDate, sortByName, sortByStartDate, sortByTeamSize, sortByTechnology } from "./SortHelper";
 
 
 interface SortProps {
@@ -34,66 +35,16 @@ export const Sort = ({projectData, setSortedProjects}: SortProps) => {
         if (!projectData) return
         if (projectData.length === 0) return
 
-        if (sortBy ===sortingOptions.startDate) {
-            projectData.sort((a, b) => {
-                if (a.dates.start && b.dates.start) {
-                    return a.dates.start.getTime() - b.dates.start.getTime()
-                } else if (a.dates.start && !b.dates.start) {
-                    return 1
-                } else if (!a.dates.start && b.dates.start) {
-                    return -1
-                } else {
-                    return 0
-                }
-            })
+        if (sortBy === sortingOptions.startDate) {
+            projectData.sort(sortByStartDate)
         } else if (sortBy === sortingOptions.endDate) {
-            projectData.sort((a, b) => {
-                if (a.dates.end === "Present" && b.dates.end === "Present") {
-                    return 0
-                } else if (a.dates.end === "Present") {
-                    return -1
-                }
-                else if (b.dates.end === "Present") {
-                    return 1
-                }
-                const aDate = a.dates.end || a.dates.start
-                const bDate = b.dates.end || b.dates.start
-                if (aDate && bDate) {
-                    return aDate.getTime() - bDate.getTime()
-                } else if (aDate && !bDate) {
-                    return 1
-                } else if (!aDate && bDate) {
-                    return -1
-                } else {
-                    return 0
-                }
-            })
+            projectData.sort(sortByEndDate)
         } else if (sortBy === sortingOptions.name) {
-          projectData.sort((a, b) => a.heading.localeCompare(b.heading))
+          projectData.sort(sortByName)
         } else if (sortBy === sortingOptions.teamSize) {
-          projectData.sort((a, b) => {
-            if (a.teamSize && b.teamSize) {
-                return b.teamSize - a.teamSize
-            } else if (a.teamSize && !b.teamSize) {
-                return 1
-            } else if (!a.teamSize && b.teamSize) {
-                return -1
-            } else {
-                return 0
-            }
-          })
+          projectData.sort(sortByTeamSize)
         } else if (sortBy === sortingOptions.technology) {
-          projectData.sort((a, b) => {
-            if (a.technologies && b.technologies) {
-                return b.technologies.length - a.technologies.length
-            } else if (a.technologies && !b.technologies) {
-                return 1
-            } else if (!a.technologies && b.technologies) {
-                return -1
-            } else {
-                return 0
-            }
-          })
+          projectData.sort(sortByTechnology)
         }
 
         if (!ascending) {
