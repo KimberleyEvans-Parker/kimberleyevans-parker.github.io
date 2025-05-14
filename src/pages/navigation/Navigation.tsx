a39295beb0ba8406f98f1443044b556d16fe6123
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-05-15 00:37:45 +0100

    Remove unused imports

--- src/pages/navigation/Navigation.tsx
+++ src/pages/navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Link, useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { SMALL_SCREEN } from "../../helpers/Constants"
 import { NavbarLink } from "./NavbarLink"
 import { NavbarExternalLink } from "./NavbarExternalLink"
