06a1f23cb5d1522168933d06963822f7f2ca775c
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-05-20 23:52:55 +0100

    Fix cosplay heading

--- src/pages/hobbies/Cosplays.tsx
+++ src/pages/hobbies/Cosplays.tsx
@@ -9,7 +9,7 @@ export const Cosplays = () => {
   return (
     <div className="content-container experience-container">
       <div className="fade left">
-        <h1>Lego</h1>
+        <h1>Cosplays</h1>
         <p>
           I enjoy sewing, making costumes and looking cool in them ^.^ I often
           design, sew and create my dresses from scratch. Some of my favourite
