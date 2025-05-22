ae1b2bae17f69192b7ecc90d696722177bf46b35
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-05-22 11:06:35 +0100

    Format files

--- src/pages/components/content item/LinksSection.tsx
+++ src/pages/components/content item/LinksSection.tsx
@@ -1,40 +1,40 @@
 import { Link } from "react-router-dom"
 import { LinksType } from "../../../data/types"
 
-export const LinksSection = (links? : LinksType) => {
+export const LinksSection = (links?: LinksType) => {
     if (!links) return <></>
 
     return (
         <>
-      {links?.linkedIn && (
-        <a href={links?.linkedIn} className={"popout-link"}>
-          <i className="fa fa-linkedin popout" aria-hidden="true" />
-          <p>
-            See the LinkedIn post here!
-          </p>
-        </a>
-      )}
-      {links?.seeMore && (
-        <p>
-          <a href={links?.seeMore}>
-            For more information, take a look here
-          </a>
-        </p>
-      )}
-      {links?.project && (
-        <p>
-          <Link to={`/experience/projects#${links?.project}`}>
-            See the project here
-          </Link>
-        </p>
-      )}
-      {links?.local && (
-        <p>
-          <Link to={links?.local}>
-            See more here
-          </Link>
-        </p>
-      )}
-      </>
+            {links?.linkedIn && (
+                <a href={links?.linkedIn} className={"popout-link"}>
+                    <i className="fa fa-linkedin popout" aria-hidden="true" />
+                    <p>
+                        See the LinkedIn post here!
+                    </p>
+                </a>
+            )}
+            {links?.seeMore && (
+                <p>
+                    <a href={links?.seeMore}>
+                        For more information, take a look here
+                    </a>
+                </p>
+            )}
+            {links?.project && (
+                <p>
+                    <Link to={`/experience/projects#${links?.project}`}>
+                        See the project here
+                    </Link>
+                </p>
+            )}
+            {links?.local && (
+                <p>
+                    <Link to={links?.local}>
+                        See more here
+                    </Link>
+                </p>
+            )}
+        </>
     )
 }
