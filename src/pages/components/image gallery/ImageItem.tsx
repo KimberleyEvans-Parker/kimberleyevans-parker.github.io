a78deecb96c9157b4847edf3f1839ba53adc2b56
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-05-29 11:59:18 +0100

    Add video support to the image gallery

--- src/pages/components/image gallery/ImageItem.tsx
+++ src/pages/components/image gallery/ImageItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LegoVideo from "../../../assets/hobbies/Lego/Elven Village.mp4";
 
 interface ImageItemProps {
   name: string;
@@ -22,22 +23,33 @@ export const ImageItem = ({name, image, animationDelay}: ImageItemProps) => {
     }
   }, [isVisible]);
   
-
   return (
-    <div className={`image-gallery-image  ${isVisible ? "is-visible" : ""} `}>
-      <img
-        ref={domRef}
-        alt={name}
-        src={image}
-        className="image-gallery-animation"
-        style={{
-          WebkitAnimationDelay: animationDelay,
-          MozAnimationDelay: animationDelay,
-          OAnimationDelay: animationDelay,
-          animationDelay: animationDelay,
-          width: "100%",
-        }}
-      />
+    <div 
+      className={`image-gallery-item image-gallery-animation ${isVisible ? "is-visible" : ""} `}
+      ref={domRef}
+      style={{
+        WebkitAnimationDelay: animationDelay,
+        MozAnimationDelay: animationDelay,
+        OAnimationDelay: animationDelay,
+        animationDelay: animationDelay,
+      }}
+    >
+      {image.match(/\.(mp4|webm|ogg)$/i) ? (
+        <video
+          muted
+          autoPlay
+          loop
+          src={LegoVideo}
+          aria-label={name}
+          className="image-gallery-image"
+        />
+      ) : (
+        <img
+          alt={name}
+          src={image}
+          className="image-gallery-image"
+        />
+      )}
     </div>
   );
 }
