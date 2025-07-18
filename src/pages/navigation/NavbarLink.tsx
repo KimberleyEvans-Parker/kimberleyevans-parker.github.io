8451c9ddbddee047bf739401e5970af1c5d5953d
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-07-18 21:00:43 +0200

    Add subheadings to NavbarLink

--- src/pages/navigation/NavbarLink.tsx
+++ src/pages/navigation/NavbarLink.tsx
@@ -11,18 +11,36 @@ export const NavbarLink = ({ heading, setDropdownOpen, subheadings }: NavbarLink
   const url = `/${heading.toLowerCase()}`;
 
   return (
-    <Link
-      className="nav-link"
-      to={url}
-      onClick={() => setDropdownOpen(false)}
-    >
-      <li
-        className={`dropdown-item 
+    <>
+      <Link
+        className="nav-link"
+        to={url}
+        onClick={() => setDropdownOpen(false)}
+      >
+        <li
+          className={`dropdown-item 
           ${location.pathname.startsWith(url) ? "active" : ""}
           `}
-      >
-        {heading}
-      </li>
-    </Link>
+        >
+          {heading}
+        </li>
+      </Link>
+
+      {subheadings && subheadings.map((subheading) => (
+        <Link
+          key={subheading}
+          to={`${url}/${subheading.toLowerCase()}`}
+          onClick={() => setDropdownOpen(false)}
+        >
+          <li
+            className={`dropdown-item sub-dropdown-item
+          ${location.pathname.startsWith(url) ? "active" : ""}
+          `}
+          >
+            {subheading}
+          </li>
+        </Link>
+      ))}
+    </>
   );
 }
