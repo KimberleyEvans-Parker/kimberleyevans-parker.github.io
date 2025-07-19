39b634161d3092ee65c52e6c64c32447fb874e07
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-07-19 18:05:43 +0200

    Remove unused vars

--- src/pages/navigation/NavbarLink.tsx
+++ src/pages/navigation/NavbarLink.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { SMALL_SCREEN } from "../../helpers/Constants";
 import { DropdownIcon } from "./DropdownIcon";
 
 interface NavbarLinkProps {
@@ -12,28 +11,8 @@ interface NavbarLinkProps {
 export const NavbarLink = ({ heading, setDropdownOpen, subheadings }: NavbarLinkProps) => {
   const location = useLocation();
   const url = `/${heading.toLowerCase()}`;
+  const isActive = location.pathname.startsWith(url);
   const [subDropdownOpen, setSubDropdownOpen] = useState(false);
-  
-    /* Keeps track of the window dimensions.  Updates when window resizes */
-    const [dimensions, setDimensions] = useState({
-      height: window.innerHeight,
-      width: window.innerWidth,
-    });
-    useEffect(() => {
-      const handleResize = () => {
-        setDimensions({
-          height: window.innerHeight,
-          width: window.innerWidth,
-        });
-      }
-  
-      window.addEventListener("resize", handleResize);
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    });
-
-    const isActive = location.pathname.startsWith(url);
 
   return (
     <>
