80a8fe53d453feee89888932daf8604a48e390af
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-08-01 23:58:48 +0200

    Remove useEffect

--- src/pages/navigation/DropdownIcon.tsx
+++ src/pages/navigation/DropdownIcon.tsx
@@ -10,29 +10,8 @@ interface DropdownIconProps {
 }
 
 export const DropdownIcon = ({ isActive, subheadings, subDropdownOpen, setSubDropdownOpen }: DropdownIconProps) => {
-    /* Keeps track of the window dimensions.  Updates when window resizes */
-    const [dimensions, setDimensions] = useState({
-        height: window.innerHeight,
-        width: window.innerWidth,
-    });
-    useEffect(() => {
-        const handleResize = () => {
-            setDimensions({
-                height: window.innerHeight,
-                width: window.innerWidth,
-            })
-        }
-
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    });
-
-
     if (!subheadings) return null
     if (!subheadings.length) return null
-    if (dimensions.width >= SMALL_SCREEN) return null
 
     return (
         <li
