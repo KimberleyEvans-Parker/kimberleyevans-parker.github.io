64e5b6264fb3cf560d5c4c8ad5a368141eb4d120
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-08-05 22:05:10 +0200

    Layout uses media queries

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,36 +1,14 @@
-import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { Navigation } from "./pages/navigation/Navigation";
 import { SubNavbar } from "./pages/navigation/SubNavbar";
-import { SMALL_SCREEN } from "./helpers/Constants";
 
 const Layout = () => {
-  /* Keeps track of the window dimensions.  Updates when window resizes */
-  const [dimensions, setDimensions] = React.useState({
-    height: window.innerHeight,
-    width: window.innerWidth,
-  });
-  React.useEffect(() => {
-    const handleResize = () => {
-      setDimensions({
-        height: window.innerHeight,
-        width: window.innerWidth,
-      });
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
-  
   const location = useLocation();
   const path = location.pathname;
 
   const showNav = path !== "/";
-  const smallScreen = dimensions.width < SMALL_SCREEN;
-  const showExperienceSub = path.startsWith("/experience") && !smallScreen;
-  const showHobbiesSub = path.startsWith("/hobbies") && !smallScreen;
+  const showExperienceSub = path.startsWith("/experience")
+  const showHobbiesSub = path.startsWith("/hobbies")
 
   return (
     <>
