a8b1b2e66f3ca315efd7965714a30e12f3abe29c
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-08-13 01:51:43 +0200

    Set up configs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+/// <reference types="vitest" />
+
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    globals: true,
+    coverage: { reporter: ['text', 'html', 'lcov'] },
+  },
 })
