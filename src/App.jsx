304de27e89de581c704dcf3b2ec48f6f7f1ad556
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2025-08-18 02:09:19 +0200

    copy 404 on build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
