cf7f3a0948b37b9233237394dbbfc3449cb71c99
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2026-02-16 06:41:26 +0000

    Create Creations.tsx

---
+++ src/data/creations.ts
@@ -0,0 +1,17 @@
+import { ContentItemType } from "./types"
+import { importAllPhotosFromFolder } from "../helpers/Helpers"
+
+const RimuBookshelf = importAllPhotosFromFolder("creations/Rimu Bookshelf")
+
+
+export const creationsData: ContentItemType[] = [
+    {
+        heading: "Rimu Bookshelf",
+        subheading: "Woodwork",
+        dates: { start: new Date(2019, 1) },
+        images: RimuBookshelf,
+        description: `I designed and created a bookshelf out of spare rimu wood.  
+            The process involved designing the bookshelf, cutting the wood to size, sanding the pieces, and then routing some of the pieces to add some decorative elements.
+            I oiled and waxed the wood to protect it and bring out its natural beauty, and then assembled the bookshelf using screws. `,
+    },
+]
---
+++ src/pages/hobbies/Creations.tsx
@@ -0,0 +1,32 @@
+import { ContentItem } from "../components/content item/ContentItem"
+import { Modal } from "../components/modal/Modal"
+import { creationsData } from "../../data/creations"
+import { isImageOnLeft } from "../../helpers/Helpers"
+
+export const Creations = () => {
+  let animationDelay = 0
+
+  const getAnimationDelay = () => {
+    const animationDelaySeconds = animationDelay.toString() + "s"
+    animationDelay += (2 - animationDelay) / 5
+    return animationDelaySeconds
+  }
+
+  return (
+    <div className="content-container">
+      <h1 className="fade left">Artistic Creations</h1>
+      <p>Aside from my other hobbies, I have enjoyed creating and developing other projects and skills, some of which you can see here :)</p>
+
+      <Modal />
+      
+      {creationsData.map((creation, index) => (
+        <ContentItem
+          key={creation.heading + creation.subheading}
+          imgOnLeft={isImageOnLeft(index)}
+          contentData={creation}
+          animationDelay={getAnimationDelay()}
+        />
+      ))}
+    </div>
+  )
+}
