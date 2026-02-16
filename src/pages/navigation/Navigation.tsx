fa6979b005df0c135c2eff452696c4d5b0b01cd5
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2026-02-16 06:47:34 +0000

    Navigate to bookshelf

--- src/pages/navigation/Navigation.tsx
+++ src/pages/navigation/Navigation.tsx
@@ -78,6 +78,7 @@ export const Navigation = () => {
                 "Art",
                 "Travel",
                 "Piano",
+                "Creations",
               ]}
             />
             <NavbarExternalLink
