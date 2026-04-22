7799bb1d286a2464a06c2daf267e9d1466300e20
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2026-04-22 23:47:16 +0100

    Add Hobbiton photo to nz

--- src/pages/hobbies/travel/TravelMap.tsx
+++ src/pages/hobbies/travel/TravelMap.tsx
@@ -4,12 +4,17 @@ import './map.css'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 import L from 'leaflet'
+import { Modal } from '../../components/modal/Modal'
+import { ModalState } from '../../../redux/state'
 import { SelectMapTerrain } from './SelectMapTerrain'
+import { getIndexOfImage } from '../../../helpers/Helpers'
 import { mapTerrains } from '../../../data/maps'
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+import { setImage } from '../../../redux/actions'
 import { travelLocations } from '../../../data/travel'
+import { useDispatch } from 'react-redux'
 import { useState } from 'react'
 
 delete (L.Icon.Default.prototype as any)._getIconUrl
@@ -22,6 +27,23 @@ L.Icon.Default.mergeOptions({
 
 
 export const TravelMap = () => {
+  
+  const dispatch = useDispatch()
+
+  const openModal = (name: string) => {
+    const allImages = travelLocations.map(loc => loc.image).filter(image => image !== undefined) as string[]
+      const imageIndex = allImages.findIndex(i => i === name)
+
+      const newModalState: ModalState = {
+        images: allImages,
+        selectedImage: imageIndex,
+        captionHeader: "",
+      }
+
+      console.log(newModalState)
+      dispatch(setImage(newModalState))
+  }
+  
   const ICON_MARKER = "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png"
   const ICON_SHADOW = "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png"
 
@@ -51,6 +73,20 @@ export const TravelMap = () => {
             <Popup>
               <strong>{location.country}{location.city && `, ${location.city}`}</strong><br />
               {location.description}
+              {location.image && 
+              <button
+                onClick={() => openModal(location.image as string)}
+                // style={{
+                >
+              <img 
+              src={location.image} 
+              alt={`${location.city || location.country}`} 
+              style={{ width: '100%', marginTop: '5px' }} 
+              // onClick={() => openModal(location.image as string)}
+              />
+
+                </button>
+              }
             </Popup>
           </Marker>
         ))}
