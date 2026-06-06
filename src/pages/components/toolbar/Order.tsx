cb3f53c333b4c1f4ea3da07a37184655157818bc
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2026-06-06 19:44:15 +0100

    Convert toolbar to modules

--- src/pages/components/toolbar/Order.tsx
+++ src/pages/components/toolbar/Order.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { ContentItemType } from "../../../data/types"
-import "./sort.css"
+import "./sort.module.css"
 
 
 interface OrderProps {
