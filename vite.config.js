ea2360f645fc39645c5f50dc8d687e2ed65e82e4
Kimberley <45865186+KimberleyEvans-Parker@users.noreply.github.com>
2026-06-13 00:56:30 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import { copyFileSync } from 'node:fs'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'node:fs'
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     {
